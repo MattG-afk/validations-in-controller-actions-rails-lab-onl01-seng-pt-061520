98886813e2c179755a6d3c6329982fe521774147
Matt Gordon <bigmattsize21@gmail.com>
2020-11-06 21:39:47 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,8 +10,8 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.valid?
-    redirect_to author_path(@author)	      @author.save
-      redirect_to author_path(@author)
+    redirect_to author_path(@author)	      
+    @author.save
     else
       render :new
     end
